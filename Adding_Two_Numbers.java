22c8a9aebec48b0200e50c269855fec004396f0c
SaiManidharKanthati <119723865+SaiManidharKanthati@users.noreply.github.com>
2024-01-31 12:15:49 +0530

    import java.util.Scanner; public class sai{     public static void main(String[] args) {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c;         c=a+b;         System.out.println(a+b);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class sai{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c;
+        c=a+b;
+        System.out.println(a+b);
+    }
+}
